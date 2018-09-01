56420177cb75337e8552651c741607acb5cd0128
Jose Tongson <kanakanadelta@gmail.com>
2018-08-31 17:08:54 -0700

    trying to fix database find and get req

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 var mongoose = require ('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+mongoose.connect('mongodb://127.0.0.1:27017/sonics');
 
 var db = mongoose.connection;
 
@@ -7,3 +7,38 @@ var db = mongoose.connection;
 db.on('error', () => {
   console.log('mongoose connection error');
 })
+
+db.once('open', () => {
+  console.log('mongoose connection success');
+})
+
+var cardSchema = mongoose.Schema({
+  id: Number,
+  name: String,
+  rarity: String,
+  html_url: String
+});
+
+var Card = mongoose.model('Card', cardSchema);
+
+const cardPull = (callback) => {
+  // Card.find({}, (err, sonics) => {
+  //   if (err) {
+  //     callback(err, null);
+  //   } else {
+  //     console.log('in cardPull', sonics)
+  //     callback(null, sonics);
+  //   }
+  // })
+  Card.find({})
+    .exec((err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(data);
+        callback(null, data);
+      }
+    })
+};
+
+module.exports.cardPull = cardPull;
--- server/index.js
+++ server/index.js
@@ -5,12 +5,24 @@ const db = require('../database/index.js');
 var app = express();
 
 app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(Parser.urlencoded({ extended: false }));
 
 //app.get
 //summon and collection
+app.get('/summon', function (req, res) {
+  db.cardPull(function(err, sonics) {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      console.log('Sonic OC summon!')
+      console.log(sonics)
+      res.json(sonics);
+    }
+  });
+});
 
 let port = 9000;
 
 app.listen(port, function() {
-  console.log(`listening on port: ${port}!`);
+  console.log(`\n it's over (port) ${port}!`);
 });
